99c3342796a63208cc9aff2ea691bcd939c2c163
Meet Pandya <pandyama2@gmail.com>
2021-06-21 22:27:01 -0400

    Created Nodejs Express app with Pug view engine

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  console.log(req.body.url);
+
+  axios.get(req.body.url)
+    .then((res)=>{
+      console.log(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+  console.log("users end point called");
+  res.send('respond with a resource');
+});
+
+module.exports = router;
