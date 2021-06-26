7488e441fc527b2c32185b0af5f931006fec8a79
Meet Pandya <pandyama2@gmail.com>
2021-06-25 22:28:10 -0400

    Added response time, status and response size calculation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const axios = require("axios").default;
+
+axios.interceptors.request.use(x => {
+  x.customData = x.customData || {}
+  x.customData.startTime = new Date().getTime();
+  return x;
+});
+
+axios.interceptors.response.use(x => {
+  x.customData = x.customData || {}
+  x.customData.time = new Date().getTime() - x.config.customData.startTime
+  console.log(`Executed in - ${x.customData.time} ms`);
+  // console.log(`Executed in ${x.config.url} - ${new Date().getTime() - x.config.requestStartedAt} ms`);
+  return x;
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mailman' });
+router.get("/", function (req, res, next) {
+  console.log("Route / called render with Mailman");
+  res.render("index", { title: "Mailman" });
+});
+
+router.post("/data", function (req, res, next) {
+  var started = new Date().getTime();
+  console.log(req.body.url);
+  var result;
+  axios
+    .get(req.body.url)
+    .then((res2) => {
+      console.log(res2.data);
+      result = res2;
+      console.log(JSON.stringify(res2.data).length + JSON.stringify(res2.headers).length + " B");
+      console.log(`Difference - ${new Date().getTime() - started} ms`);
+      var timeTaken = new Date().getTime() - started;
+      var size = JSON.stringify(res2.data).length + JSON.stringify(res2.headers).length;
+      res.render("index", { response: res2.data, status: "OK", time: timeTaken, length: size});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("index", { response: "Request Failed"});
+    });
+
+  console.log("users end point called");
+  // res.render("index", { response: "No Data"});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,20 @@ var router = express.Router();
 const axios = require('axios');
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  console.log(req.body.url);
+// router.post('/', function(req, res, next) {
+//   console.log(req.body.url);
 
-  axios.get(req.body.url)
-    .then((res)=>{
-      console.log(res.data);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+//   axios.get(req.body.url)
+//     .then((res)=>{
+//       console.log(res.data);
+//     })
+//     .catch((err)=>{
+//       console.log(err);
+//     })
 
-  console.log("users end point called");
-  res.send('respond with a resource');
-});
+//   console.log("users end point called");
+//   // res.send('respond with a resource');
+//   res.render('index', { response: res.data });
+// });
 
 module.exports = router;
